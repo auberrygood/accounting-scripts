9ea87b8777f4ddbd8b104790d94419bd37f8f0df
Aubrey <aubrey.sandrs@gmail.com>
2021-03-30 17:31:46 -0700

    rewrote melons_info.py's program to reflect melons.py's new single dictionary format

--- melon_info.py
+++ melon_info.py
@@ -1,18 +1,31 @@
 """Print out all the melons in our inventory."""
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
+# from melons import melon_names, melon_seedlessness, melon_prices
+from melons import melons
+
+# def print_melon(melon_name):
+#     """Print each melon with corresponding attribute information."""
+
+#     # have_or_have_not = 'have'
+#     # if seedless:
+#     #     have_or_have_not = 'do not have'
+#     melon_info[]
+
+#     # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
-
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+#     print (f'{melon}'s  )
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+# for melon in melon_info:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+for melon in melons:
+    print (melon)
+    print (f"   price: ${melons[melon]['price']:.2f}")
+    print (f"   seedless: {melons[melon]['seedless']}")
+    print (f"   flesh color: {melons[melon]['flesh_color']}")
+    print (f"   rind color: {melons[melon]['rind_color']}")
+    print (f"   weight: {melons[melon]['weight']}")
+    # print ()
