91146058ebdc3d6e35163604bf71ac4596e6d6ff
Aubrey <aubrey.sandrs@gmail.com>
2021-03-30 17:33:22 -0700

    cleaned up some spacing

--- melon_info.py
+++ melon_info.py
@@ -15,9 +15,6 @@ from melons import melons
 #     # print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
-#     print (f'{melon}'s  )
-
-
 # for melon in melon_info:
 #     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
 
@@ -28,4 +25,4 @@ for melon in melons:
     print (f"   flesh color: {melons[melon]['flesh_color']}")
     print (f"   rind color: {melons[melon]['rind_color']}")
     print (f"   weight: {melons[melon]['weight']}")
-    # print ()
+    print ()
